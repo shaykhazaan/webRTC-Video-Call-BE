b1077196ad998d059a690dfb7fa0d58b7cdb0f53
azancendrol <azan.cendrol@gmail.com>
2024-01-26 15:16:16 +0530

    http and socket.io server setup

--- index.ts
+++ index.ts
@@ -1,9 +1,36 @@
 import http from "http";
 import express, { Express } from "express";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app: Express = express();
 const server: http.Server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+app.use(cors());
 
-server.listen(2020, () => {
-  console.log("server running ");
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser"), { signal: signalData, from, name };
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+const PORT = 2020;
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
